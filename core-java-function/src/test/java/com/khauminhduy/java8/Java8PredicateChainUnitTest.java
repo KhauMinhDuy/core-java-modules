d22112900c859f9556a20ff6c57773219d4f21ee
khauminhduy <khauminhduy@gmail.com>
2022-06-22 09:49:25 +0700

    update

--- core-java-function/src/test/java/com/khauminhduy/java8/Java8PredicateChainUnitTest.java
+++ core-java-function/src/test/java/com/khauminhduy/java8/Java8PredicateChainUnitTest.java
@@ -1,14 +1,15 @@
 package com.khauminhduy.java8;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class Java8PredicateChainUnitTest {
@@ -19,10 +20,10 @@ public class Java8PredicateChainUnitTest {
 	public void whenFilterList_thenSuccess() {
 		List<String> result = names.stream()
 			.filter(name -> name.startsWith("A"))
-			.collect(Collectors.toList());
+			.collect(toList());
 		
 		assertEquals(2, result.size());
-		MatcherAssert.assertThat(result, Matchers.contains("Adam", "Alexander"));
+		assertThat(result, contains("Adam", "Alexander"));
 	}
 	
 	@Test
@@ -30,32 +31,99 @@ public class Java8PredicateChainUnitTest {
 		List<String> result = names.stream()
 			.filter(name -> name.startsWith("A"))
 			.filter(name -> name.length() < 5)
-			.collect(Collectors.toList());
+			.collect(toList());
 		
 		assertEquals(1, result.size());
-		MatcherAssert.assertThat(result, Matchers.contains("Adam"));
+		assertThat(result, contains("Adam"));
 	}
 	
 	@Test
 	public void whenFilterListWithComplexPredicate_thenSuccess() {
 		List<String> result = names.stream()
 			.filter(name -> name.startsWith("A") && name.length() < 5)
-			.collect(Collectors.toList());
+			.collect(toList());
 		
 		assertEquals(1, result.size());
-		MatcherAssert.assertThat(result, Matchers.contains("Adam"));
+		assertThat(result, contains("Adam"));
 	}
 	
 	@Test
 	public void whenFilterListWithCombinedPredicatesInline_thenSuccess() {
-		Predicate<String> predicate1 = name -> name.startsWith("A");
-		Predicate<String> predicate2 = name -> name.length() < 5;
 		List<String> result = names.stream()
-			.filter(predicate1.and(predicate2))
-			.collect(Collectors.toList());
+      .filter(((Predicate<String>) name -> name.startsWith("A")).and(name -> name.length() < 5))
+      .collect(toList());
+    
+    assertEquals(1, result.size());
+    assertThat(result, contains("Adam"));
+	}
+	
+	@Test
+	public void whenFilterListWithCombinedPredicatesUsingAnd_thenSuccess() {
+		Predicate<String> predicate1 = str -> str.startsWith("A");
+    Predicate<String> predicate2 = str -> str.length() < 5;
+
+    List<String> result = names.stream()
+      .filter(predicate1.and(predicate2))
+      .collect(toList());
+
+    assertEquals(1, result.size());
+    assertThat(result, contains("Adam"));
+	}
+	
+	@Test
+	public void whenFilterListWithCombinedPredicatesUsingOr_thenSuccess() {
+		Predicate<String> predicate1 = name -> name.startsWith("J");
+		Predicate<String> predicate2 = name -> name.length() < 4;
+		
+		List<String> result = names.stream()
+			.filter(predicate1.or(predicate2))
+			.collect(toList());
+		
+		assertEquals(2, result.size());
+		assertThat(result, contains("John", "Tom"));
+	}
+	
+	@Test
+	public void whenFilterListWithCombinedPredicatesUsingOrAndNegate_thenSuccess() {
+		Predicate<String> predicate1 = name -> name.startsWith("J");
+		Predicate<String> predicate2 = name -> name.length() < 4;
+		
+		List<String> result = names.stream()
+				.filter(predicate1.or(predicate2.negate()))
+				.collect(toList());
+			
+			assertEquals(3, result.size());
+			assertThat(result, contains( "Adam", "Alexander", "John"));
+	}
+	
+	@Test
+	public void whenFilterListWithCollectionOfPredicatesUsingAnd_thenSuccess() {
+		List<Predicate<String>> allPredicates = new ArrayList<>();
+		allPredicates.add(name -> name.startsWith("A"));
+		allPredicates.add(name -> name.contains("d"));
+		allPredicates.add(name -> name.length() > 4);
+		
+		List<String> result = names.stream()
+			.filter(allPredicates.stream().reduce(name -> true, Predicate::and))
+			.collect(toList());
 		
 		assertEquals(1, result.size());
-		MatcherAssert.assertThat(result, Matchers.contains("Adam"));
+		assertThat(result, contains("Alexander"));
+	}
+	
+	@Test
+	public void whenFilterListWithCollectionOfPredicatesUsingOr_thenSuccess() {
+		List<Predicate<String>> allPredicates = new ArrayList<>();
+		allPredicates.add(name -> name.startsWith("A"));
+		allPredicates.add(name -> name.contains("d"));
+		allPredicates.add(name -> name.length() > 4);
+		
+		List<String> result = names.stream()
+        .filter(allPredicates.stream().reduce(x -> false, Predicate::or))
+        .collect(toList());
+
+    assertEquals(2, result.size());
+    assertThat(result, contains("Adam", "Alexander"));
 	}
 	
 }
